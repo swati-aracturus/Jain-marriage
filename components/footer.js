983dcf90662e3952ebd9cf6c2d5820f6ca60f4d8
swati mandal <158429232+swati-aracturus@users.noreply.github.com>
2024-05-26 11:29:04 +0530

    .

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
     <>
       <section className="container w-5/6 ps:!w-[95%] mx-auto pt-[5%] pd:!pt-[20%] h-auto">
         <div className="flex flex-wrap justify-center items-stretch pb-10">
-          <div className="bg-[#080A52]  w-full md:w-3/5 px-4 ps:px-2 py-6 order-2 md:order-none flex flex-col justify-center">
+          <div className="bg-[#080A52]  w-full md:w-3/5 px-4 ps:px-2 py-4 order-2 md:order-none flex flex-col justify-center">
             <div className="flex pt-[4%]  justify-center items-center">
               <p className="text-4xl text-white ps:!text-[20px] pm:text-[20px] pm:font-[550] font-semibold">
                 WHY CHOOSE US
@@ -31,26 +31,26 @@ export default function Footer() {
             </div>
 
             <div className="md:flex ps:flex content-center flex-wrap -mx-2 p-3 pb-[2%] bg-grey rounded shadow-lg">
+              <div className="ps:flex ps:w-1/2 lg:w-1/4 px-2 py-2 justify-center align-center items-center">
+                <div className="ps:flex-1 p-4 rounded  backdrop-blur-[2px] bg-white/25  text-center">
+                  <h1 className="text-2xl font-bold text-[#EB2188]"> 7+ </h1>
+                  <p className=" text-white"> Years of bond of trust </p>
+                </div>
+              </div>
               <div className="ps:flex ps:w-1/2 lg:w-1/4 px-2 py-2 ">
-                <div className="ps:flex-1 p-4 rounded shadow-lg bg-white border-b border-r border-grey-dark text-center">
-                  <h1>Squid </h1>
-                  <p>Squid palo santo </p>
+                <div className="ps:flex-1 p-4 rounded  backdrop-blur-[2px] bg-white/25  text-center">
+                  <h1 className="text-2xl font-bold text-[#EB2188]"> 10,000+ </h1>
+                  <p className=" text-white">Number of Live Profiles </p>
                 </div>
-              </div>
-              <div className="ps:flex ps:w-1/2 lg:w-1/4 px-2 py-2">
-                <div className="ps:flex-1 p-4 rounded shadow-lg bg-white border-b border-r border-grey-dark text-center">
-                  <h1>Squid </h1>
-                  <p>Squid palo santo </p>{" "}
+              </div><div className="ps:flex ps:w-1/2 lg:w-1/4 px-2 py-2 ">
+                <div className="ps:flex-1 p-4 rounded  backdrop-blur-[2px] bg-white/25  text-center">
+                  <h1 className="text-2xl font-bold text-[#EB2188]"> 50,000+ </h1>
+                  <p className=" text-white">Number of Visited Profiles </p>
                 </div>
-              </div>
-              <div className="md:flex ps:w-1/2 lg:w-1/4 px-2 py-2">
-                <div className="md:flex-1 p-4 rounded shadow-lg bg-white border-b border-r border-grey-dark text-center">
-                  <h1>Squid </h1> <p>Squid palo santo </p>{" "}
-                </div>
-              </div>
-              <div className="md:flex ps:w-1/2 lg:w-1/4 px-2 py-2">
-                <div className="md:flex-1 p-4 rounded shadow-lg bg-white border-b border-r border-grey-dark text-center">
-                  <h1>Squid </h1> <p>Squid palo santo </p>{" "}
+              </div><div className="ps:flex ps:w-1/2 lg:w-1/4 px-2 py-2 ">
+                <div className="ps:flex-1 p-4 rounded  backdrop-blur-[2px] bg-white/25  text-center">
+                  <h1 className="text-2xl font-bold text-[#EB2188]"> 9,893+ </h1>
+                  <p className=" text-white"> Successful Marriages </p>
                 </div>
               </div>
             </div>
@@ -64,6 +64,445 @@ export default function Footer() {
           </div>
         </div>
       </section>
+    <section>
+   <div className="flex justify-center text-center items-center" >
+    <div className="w-1/5 py-2 bg-[#EB2188] ">
+  <h1 className="text-white">TRUSTED BY MILLIONS</h1>
+</div>
+</div>
+
+
+
+    <div className="w-full flex justify-center text-center items-center py-2 bg-[#080A52] ">
+    <div className="w-3/5 py-2  ">
+  <div className="border-t-2 border-b-2 border-gray-500 flex justify-center text-center items-center py-8 mt-[10%]">
+<div className=""></div>
+  </div>
+  </div>
+</div>
+</section>  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     {/*  <div className="bg-gray-100 mx-auto max-w-6xl bg-white py-20 px-12 lg:px-24 shadow-xl mb-24">
+  <form>
+    <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
+      <div className="-mx-3 md:flex mb-6">
+        <div className="md:w-1/2 px-3 mb-6 md:mb-0">
+          <label
+            className="uppercase tracking-wide text-black text-xs font-bold mb-2"
+            htmlFor="company"
+          >
+           Whatsapp No.<span className="text-red-500 ">*</span>
+          </label>
+          <input
+            className="w-full  text-black border border-gray-200 rounded py-3 px-4 mb-3"
+            id="company"
+            type="text"
+            placeholder="Tutsplus"
+          />
+          <div>
+            <span className="text-red-500 text-xs italic">
+              Please fill out this field.
+            </span>
+          </div>
+        </div>
+         <div className="md:w-1/2 px-3 mb-6 md:mb-0">
+          <label
+            className="uppercase tracking-wide text-black text-xs font-bold mb-2"
+            htmlFor="company"
+          >
+          Email Id<span className="text-red-500 ">*</span>
+          </label>
+          <input
+            className="w-full  text-black border border-gray-200 rounded py-3 px-4 mb-3"
+            id="company"
+            type="text"
+            placeholder="Tutsplus"
+          />
+          <div>
+            
+          </div>
+        </div>
+     
+      </div>
+      <div className="-mx-3 md:flex mb-6">
+        <div className="md:w-1/2 px-3 mb-6 md:mb-0">
+          <label
+            className="uppercase tracking-wide text-black text-xs font-bold mb-2"
+            htmlFor="company"
+          >
+           Password<span className="text-red-500 ">*</span>
+          </label>
+          <input
+            className="w-full  text-black border border-gray-200 rounded py-3 px-4 mb-3"
+            id="company"
+            type="text"
+            placeholder="Tutsplus"
+          />
+          <div>
+           
+          </div>
+        </div>
+         <div className="md:w-1/2 px-3 mb-6 md:mb-0">
+          <label
+            className="uppercase tracking-wide text-black text-xs font-bold mb-2"
+            htmlFor="company"
+          >
+       Re-Type Password<span className="text-red-500 ">*</span>
+          </label>
+          <input
+            className="w-full  text-black border border-gray-200 rounded py-3 px-4 mb-3"
+            id="company"
+            type="text"
+            placeholder="Tutsplus"
+          />
+          <div>
+           
+          </div>
+        </div>
+     
+      </div>
+      <div className="-mx-3 md:flex mb-6">
+        <div className="md:w-1/2 px-3 mb-6 md:mb-0">
+          <label
+            className="uppercase tracking-wide text-black text-xs font-bold mb-2"
+            htmlFor="company"
+          >
+           Refrence By<span className="text-red-500 ">*</span>
+          </label>
+          <input
+            className="w-full  text-black border border-gray-200 rounded py-3 px-4 mb-3"
+            id="company"
+            type="text"
+            placeholder="Tutsplus"
+          />
+          <div>
+           
+          </div>
+        </div>
+         <div className="md:w-1/2 px-3 mb-6 md:mb-0">
+          <label
+            className="uppercase tracking-wide text-black text-xs font-bold mb-2"
+            htmlFor="company"
+          >
+        Religion<span className="text-red-500 ">*</span>
+          </label>
+          <input
+            className="w-full  text-black border border-gray-200 rounded py-3 px-4 mb-3"
+            id="company"
+            type="text"
+            placeholder="Tutsplus"
+          />
+          <div>
+           
+          </div>
+        </div>
+     
+      </div>
+      <div className="-mx-3 md:flex mb-6">
+        <div className="md:w-1/2 px-3 mb-6 md:mb-0">
+          <label
+            className="uppercase tracking-wide text-black text-xs font-bold mb-2"
+            htmlFor="company"
+          >
+          Community<span className="text-red-500 ">*</span>
+          </label>
+          <input
+            className="w-full  text-black border border-gray-200 rounded py-3 px-4 mb-3"
+            id="company"
+            type="text"
+            placeholder="Tutsplus"
+          />
+          <div>
+           
+          </div>
+        </div>
+         <div className="md:w-1/2 px-3 mb-6 md:mb-0">
+          <label
+            className="uppercase tracking-wide text-black text-xs font-bold mb-2"
+            htmlFor="company"
+          >
+        Gender<span className="text-red-500 ">*</span>
+          </label>
+          <input
+            className="w-full  text-black border border-gray-200 rounded py-3 px-4 mb-3"
+            id="company"
+            type="text"
+            placeholder="Tutsplus"
+          />
+          <div>
+           
+          </div>
+        </div>
+     
+      </div>
+    
+      <div className="-mx-3 md:flex mb-6">
+        <div className="md:w-1/2 px-3 mb-6 md:mb-0">
+          <label
+            className="uppercase tracking-wide text-black text-xs font-bold mb-2"
+            htmlFor="company"
+          >
+          Candidate Name<span className="text-red-500 ">*</span>
+          </label>
+          <input
+            className="w-full  text-black border border-gray-200 rounded py-3 px-4 mb-3"
+            id="company"
+            type="text"
+            placeholder="Tutsplus"
+          />
+          <div>
+           
+          </div>
+        </div>
+         <div className="md:w-1/2 px-3 mb-6 md:mb-0">
+          <label
+            className="uppercase tracking-wide text-black text-xs font-bold mb-2"
+            htmlFor="company"
+          >
+      Birth Place<span className="text-red-500 ">*</span>
+          </label>
+          <input
+            className="w-full  text-black border border-gray-200 rounded py-3 px-4 mb-3"
+            id="company"
+            type="text"
+            placeholder="Tutsplus"
+          />
+          <div>
+           
+          </div>
+        </div>
+     
+      </div>
+      <div className="-mx-3 md:flex mb-6">
+        <div className="md:w-1/2 px-3 mb-6 md:mb-0">
+          <label
+            className="uppercase tracking-wide text-black text-xs font-bold mb-2"
+            htmlFor="company"
+          >
+        Height<span className="text-red-500 ">*</span>
+          </label>
+          <input
+            className="w-full  text-black border border-gray-200 rounded py-3 px-4 mb-3"
+            id="company"
+            type="text"
+            placeholder="Tutsplus"
+          />
+          <div>
+           
+          </div>
+        </div>
+         <div className="md:w-1/2 px-3 mb-6 md:mb-0">
+          <label
+            className="uppercase tracking-wide text-black text-xs font-bold mb-2"
+            htmlFor="company"
+          >
+      Weight<span className="text-red-500 ">*</span>
+          </label>
+          <input
+            className="w-full  text-black border border-gray-200 rounded py-3 px-4 mb-3"
+            id="company"
+            type="text"
+            placeholder="Tutsplus"
+          />
+          <div>
+           
+          </div>
+        </div>
+     
+      </div>
+      <div className="-mx-3 md:flex mb-6">
+        <div className="md:w-1/2 px-3 mb-6 md:mb-0">
+          <label
+            className="uppercase tracking-wide text-black text-xs font-bold mb-2"
+            htmlFor="company"
+          >
+      Complexion<span className="text-red-500 ">*</span>
+          </label>
+          <input
+            className="w-full  text-black border border-gray-200 rounded py-3 px-4 mb-3"
+            id="company"
+            type="text"
+            placeholder="Tutsplus"
+          />
+          <div>
+           
+          </div>
+        </div>
+         <div className="md:w-1/2 px-3 mb-6 md:mb-0">
+          <label
+            className="uppercase tracking-wide text-black text-xs font-bold mb-2"
+            htmlFor="company"
+          >
+      Profession<span className="text-red-500 ">*</span>
+          </label>
+          <input
+            className="w-full  text-black border border-gray-200 rounded py-3 px-4 mb-3"
+            id="company"
+            type="text"
+            placeholder="Tutsplus"
+          />
+          <div>
+           
+          </div>
+        </div>
+     
+      </div>
+      <div className="-mx-3 md:flex mb-6">
+        <div className="md:w-1/2 px-3 mb-6 md:mb-0">
+          <label
+            className="uppercase tracking-wide text-black text-xs font-bold mb-2"
+            htmlFor="company"
+          >
+    Marrital Status<span className="text-red-500 ">*</span>
+          </label>
+          <input
+            className="w-full  text-black border border-gray-200 rounded py-3 px-4 mb-3"
+            id="company"
+            type="text"
+            placeholder="Tutsplus"
+          />
+          <div>
+           
+          </div>
+        </div>
+         <div className="md:w-1/2 px-3 mb-6 md:mb-0">
+          <label
+            className="uppercase tracking-wide text-black text-xs font-bold mb-2"
+            htmlFor="company"
+          >
+    Blood group<span className="text-red-500 ">*</span>
+          </label>
+          <input
+            className="w-full  text-black border border-gray-200 rounded py-3 px-4 mb-3"
+            id="company"
+            type="text"
+            placeholder="Tutsplus"
+          />
+          <div>
+           
+          </div>
+        </div>
+     
+      </div>
+      <label
+            className="uppercase tracking-wide text-black text-xs font-bold mb-2"
+            htmlFor="company"
+          >
+    photo<span className="text-red-500 ">*</span>
+          </label>
+
+
+          <div className="flex items-center justify-center w-full">
+  <label
+    htmlFor="dropzone-file"
+    className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-702 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+  >
+    <div className="flex flex-col items-center justify-center pt-5 pb-6">
+      <svg
+        className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 20 16"
+      >
+        <path
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+        />
+      </svg>
+      <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+        <span className="font-semibold">Click to upload</span> or drag and drop
+      </p>
+      <p className="text-xs text-gray-500 dark:text-gray-400">
+        SVG, PNG, JPG or GIF (MAX. 800x400px)
+      </p>
+    </div>
+    <input id="dropzone-file" type="file" className="hidden" />
+  </label>
+</div>
+
+<label
+            className="uppercase tracking-wide pt-8 text-black text-xs font-bold mb-2"
+            htmlFor="company"
+          >
+    Id Proof<span className="text-red-500 ">*</span>
+          </label>
+          
+
+          <div className="flex items-center justify-center w-full">
+  <label
+    htmlFor="dropzone-file"
+    className="flex flex-col items-center justify-center w-full h-64 border-2 
+    border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-100 hover:bg-gray-200 dark:border-gray-200 dark:hover:border-gray-100 dark:hover:bg-gray-200"
+  >
+    <div className="flex flex-col items-center justify-center pt-5 pb-6">
+      <svg
+        className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 20 16"
+      >
+        <path
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+        />
+      </svg>
+      <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+        <span className="font-semibold">Click to upload</span> or drag and drop
+      </p>
+      <p className="text-xs text-gray-500 dark:text-gray-400">
+        SVG, PNG, JPG or GIF (MAX. 800x400px)
+      </p>
+    </div>
+    <input id="dropzone-file" type="file" className="hidden" />
+  </label>
+</div>
+      <div className="flex justify-center py-4">
+              {" "}
+              <button
+                className="w-48 ps:w-36   pm:py-1 pm:!text-[14px] text-lg font-bold ps:!text-[16px]  py-2 text-white rounded-[10px] bg-pink-600 hover:bg-pink-700 focus:outline-none  focus:ring focus:ring-pink-400 "
+                onClick={() => console.log("Register")}
+              >
+                {" "}
+               Register Now
+              </button>
+            </div>
+    </div>
+  </form>
+</div> */}
+
     </>
   );
 }
